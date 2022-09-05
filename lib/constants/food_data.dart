31fbfb25715f032f7e40fefeb00dc5aa80805148
aratane <akunbotak503@gmail.com>
2022-09-05 13:03:43 +0700

    flutter clean

--- lib/constants/food_data.dart
+++ lib/constants/food_data.dart
@@ -83,7 +83,87 @@ class Food {
         about:
             'Serabi merupakan jajanan pasar tradisional yang berasal dari Indonesia. Serabi berasal dari bahasa Jawa yang berinduk dasar dari kata "rabi" yang dalam bahasa Jawa berarti "kawin". Mungkin karena proses pembuatannya yang cukup sebentar dan tidak terlalu lama maka orang Jawa menyebutnya dengan kata "Serabi".',
         highLight: false,
-      )
+      ),
+      Food(
+        imgUrl: 'assets/images/teh.jpg',
+        desc: 'No 4 Trending',
+        name: 'Teh Poci',
+        waitTIme: '3 menit',
+        score: 4.9,
+        cal: '325 Kalori',
+        price: 15,
+        quantity: 1,
+        ingredients: [
+          {'Nescape': 'assets/images/nescape.png'},
+          {'Nescape': 'assets/images/nescape.png'},
+          {'Nescape': 'assets/images/nescape.png'},
+          {'Nescape': 'assets/images/nescape.png'},
+          {'Nescape': 'assets/images/nescape.png'},
+        ],
+        about:
+        'Teh poci adalah teh yang diseduh secara khas dalam poci dan cangkir dari tanah liat. Minuman ini ditambah dengan gula batu dan diminum panas-panas. Minuman yang sangat disukai oleh masyarakat Tegal, Slawi, Pemalang, Brebes, dan sekitarnya.',
+        highLight: false,
+      ),
+      Food(
+        imgUrl: 'assets/images/kopi.jpg',
+        desc: 'No 5 Trending',
+        name: 'Kopi Kenangan',
+        waitTIme: '5 menit',
+        score: 4.9,
+        cal: '325 Kalori',
+        price: 25,
+        quantity: 1,
+        ingredients: [
+          {'Nescape': 'assets/images/nescape.png'},
+          {'Air Putih': 'assets/images/airputih.jpg'},
+          {'Es Batu': 'assets/images/esbatu.jpg'},
+          {'Gula Aren': 'assets/images/gulaaren.jpg'},
+          {'Susu': 'assets/images/susu.jpg'},
+        ],
+        about:
+        '“At Kopi Kenangan, our dream is to serve high quality coffee, made with the freshest local ingredients to customers across Indonesia – and the rest of the world.”',
+        highLight: false,
+      ),
+      Food(
+        imgUrl: 'assets/images/cimol.jpg',
+        desc: 'No 6 Trending',
+        name: 'Cimol',
+        waitTIme: '1 menit',
+        score: 4.5,
+        cal: '325 Kalori',
+        price: 2,
+        quantity: 1,
+        ingredients: [
+          {'Nescape': 'assets/images/nescape.png'},
+          {'Nescape': 'assets/images/nescape.png'},
+          {'Nescape': 'assets/images/nescape.png'},
+          {'Nescape': 'assets/images/nescape.png'},
+          {'Nescape': 'assets/images/nescape.png'},
+        ],
+        about:
+        'Cimol adalah makanan ringan khas Sunda yang dibuat dari tepung kanji. Cimol berasal dari kata aci digemol yang artinya tepung kanji dibentuk bulat-bulat. Cimol biasanya dijual di pinggir jalan. Cara membuatnya, adonan tepung kanji dibentuk bulat-bulat, kemudian digoreng.',
+        highLight: false,
+      ),
+      Food(
+        imgUrl: 'assets/images/surabi.jpg',
+        desc: 'No 7 Trending',
+        name: 'Surabi Haneut',
+        waitTIme: '3 menit',
+        score: 4.7,
+        cal: '325 Kalori',
+        price: 5,
+        quantity: 1,
+        ingredients: [
+          {'Nescape': 'assets/images/nescape.png'},
+          {'Nescape': 'assets/images/nescape.png'},
+          {'Nescape': 'assets/images/nescape.png'},
+          {'Nescape': 'assets/images/nescape.png'},
+          {'Nescape': 'assets/images/nescape.png'},
+        ],
+        about:
+        'Serabi merupakan jajanan pasar tradisional yang berasal dari Indonesia. Serabi berasal dari bahasa Jawa yang berinduk dasar dari kata "rabi" yang dalam bahasa Jawa berarti "kawin". Mungkin karena proses pembuatannya yang cukup sebentar dan tidak terlalu lama maka orang Jawa menyebutnya dengan kata "Serabi".',
+        highLight: false,
+      ),
     ];
   }
 
